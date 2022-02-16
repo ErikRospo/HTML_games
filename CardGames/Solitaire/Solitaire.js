19a011a915d277951539f863b28a7f3a7aa73ca8
ErikRospo <rerik.work@gmail.com>
2022-02-16 11:59:54 -0800

    updates, improvements. fixed styles

--- CardGames/Solitaire/Solitaire.js
+++ CardGames/Solitaire/Solitaire.js
@@ -2,6 +2,7 @@ const main_slots = document.querySelectorAll(".main-slot")
 const ace_slots = document.querySelectorAll(".ace-card-slot")
 const draw_slot = document.querySelector(".draw-div")
 const discard_slot = document.querySelector(".discard-div")
+const restart_button=document.querySelector(".restart");
 var main_slots_list = []
 
 function startGame() {
@@ -14,7 +15,7 @@ function startGame() {
     setup(deck);
     updateDecks();
 }
-document.addEventListener("click", () => {
+restart_button.addEventListener("click", () => {
     startGame()
 })
 startGame()
@@ -37,14 +38,14 @@ function updateDecks() {
             main_slots[index].removeChild(element);
         }
         var moveDownOffset=0
-        for (let index3=main_slots_list[index].cards.length;index3>0;index3--){
+        for (let index3=0;index3<=main_slots_list[index].cards.length;index3++){
             moveDownOffset+=20
             var elm = main_slots_list[index].pop()
             console.log(elm)
             console.log(main_slots_list[index])
             var image = document.createElement("img")
             image.className = "ImageImage";
-            if (main_slots_list[index].cards.length==1){
+            if (main_slots_list[index].cards.length==0){
                 console.log("a")
                 image.src = `../PNG-cards/${ImgIdValueGetter[CARD_VALUE_MAP[elm.value]]}_of_${ImgIdSuitGetter[elm.suit]}.png`
             }else{
